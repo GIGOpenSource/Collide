a4eadf8eb1497f33f1ea74e1443145033ff2bc0b
Chans <imagechans@gmail.com>
2025-07-28 07:04:16 +0800

    fix

---
+++ collide-common/collide-tcc/src/main/java/com/gig/collide/tcc/entity/TransActionLogState.java
@@ -0,0 +1,22 @@
+package com.gig.collide.tcc.entity;
+
+/**
+ * @author Hollis
+ */
+public enum TransActionLogState {
+
+    /**
+     * Try
+     */
+    TRY,
+
+    /**
+     * Confirm
+     */
+    CONFIRM,
+
+    /**
+     * Cancel
+     */
+    CANCEL
+}
